a78412b071eb78ae6d34a941fa97bbd0e19aad97
Ja-sonYun <killa30867@gmail.com>
2026-07-05 14:13:36 +0900

    Update version

--- pkg/cliproxyapi/default.nix
+++ pkg/cliproxyapi/default.nix
@@ -6,13 +6,13 @@
 
 buildGo126Module rec {
   pname = "cliproxyapi";
-  version = "7.2.30";
+  version = "7.2.50";
 
   src = fetchFromGitHub {
     owner = "router-for-me";
     repo = "CLIProxyAPI";
     rev = "v${version}";
-    hash = "sha256-7kdzuWJNdJf90ZCICmIx7NDN8M2cq6WLfHXUIb30qL0=";
+    hash = "sha256-MSKLk+vAVOBSvZpxalgE7hL2vOzBb7IsR4Wqt1QPYGY=";
   };
 
   vendorHash = "sha256-wrPg5VzbUS4rMpcqPVzDU0RIKHCq0/86fLi3p4DNf9Y=";
@@ -24,7 +24,7 @@ buildGo126Module rec {
     "-s"
     "-w"
     "-X main.Version=${version}"
-    "-X main.Commit=f1ed8912bbed42e72499b4065c0eb93667123f98"
+    "-X main.Commit=5afc0f1d5e9ed8d47809a1bd1f54834bc7e75375"
     "-X main.BuildDate=unknown"
   ];
 
